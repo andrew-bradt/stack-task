46fd89e16ac164b0d78ffe1bc8109acb9dadfe7d
Andr <62073230+bradttles@users.noreply.github.com>
2021-10-20 14:30:46 -0400

    Created Login, Sign Up and Todos Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,23 @@
 import './App.css';
-import {useState} from 'react';
-import Tasks from './components/Tasks';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
 
 function App() {
-  const [user_id,setUser_id] = useState(null);
-  const [page,setPage] = useState('login');
-  
-  const handleUserId = (id)=>{
-    setUser_id(id);
-    setPage('tasks');
-  }
-
-  const switchPage=(destination)=>{
-    setPage(destination);
-  }
-
-  const switchComponent = ()=>{
-    switch(page){
-      case "login":return <LoginForm onLogin={handleUserId} navToSignUp={switchPage}/>;
-      case "register":return <SignUpForm onSignUp={handleUserId} navToLogin={switchPage}/>;
-      case "tasks":return <Tasks user_id={user_id}/>;
-      default:return <LoginForm onLogin={handleUserId} navToSignUp={switchPage}/>;
-    }
-  }
-
   return (
     <div className="App">
-      {switchComponent()}
+      <Router>
+          <Switch>
+            <Route exact path="/">
+              Login
+            </Route>
+            <Route path="/sign-up">
+              Sign Up
+            </Route>
+            <Route path="/todos">
+              Todos
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
