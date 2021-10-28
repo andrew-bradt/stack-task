b0b8cbcd0bd94c436db660b2f262e3b9e98143e8
Andr <62073230+bradttles@users.noreply.github.com>
2021-10-28 15:15:34 -0400

    Changed ENV variable name from PORT to DB_PORT

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
-const { HOST, USER, PASSWORD, DATABASE, PORT } = process.env;
+const { HOST, USER, PASSWORD, DATABASE, DB_PORT } = process.env;
 
 const pool = new Pool({
   host: HOST,
   user: USER,
   password: PASSWORD,
   database: DATABASE,
-  port: PORT,
+  port: DB_PORT,
 });
 
 module.exports = pool;
