053bf07f40d90cb0f2cd0119b5ff1e5e3f1f29c2
Andr <62073230+bradttles@users.noreply.github.com>
2021-11-02 11:05:17 -0400

    Added switch case to assign pool a value based on ENVIRONMENT variable

--- server/db.js
+++ server/db.js
@@ -1,12 +1,26 @@
-const { Pool } = require('pg');
-const { HOST, USER, PASSWORD, DATABASE, DB_PORT } = process.env;
+const { Pool, Client } = require('pg');
+const { ENVIRONMENT, HOST, USER, PASSWORD, DATABASE, DB_PORT } = process.env;
 
-const pool = new Pool({
-  host: HOST,
-  user: USER,
-  password: PASSWORD,
-  database: DATABASE,
-  port: DB_PORT,
-});
+const pool = (()=>{
+  switch(ENVIRONMENT){
+    case 'development':
+      return new Pool({
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+        database: DATABASE,
+        port: DB_PORT,
+      });
+    case 'production':
+      return new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }).connect();
+    default:
+      return;
+    };
+})();
 
 module.exports = pool;
